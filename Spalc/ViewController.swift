6210e72b9ea3a49b7eda36d21330765d106a1fd9
Stuart Chapman <stubez@users.noreply.github.com>
2015-09-30 16:08:33 +0100

    Now you can perform calculations!

--- Spalc/ViewController.swift
+++ Spalc/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    enum Operation: String {
+        case Clear = "Clear"
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case None = ""
+    }
+    
     var buttonSound: AVAudioPlayer!
     
-    @IBOutlet weak var counter: UILabel!
+    var runningNumber = ""
+    var leftCalculationPart = ""
+    var rightCalculationPart = ""
+    var result = ""
+    var currentOperation = Operation.None
+    
+    @IBOutlet weak var display: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +41,70 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func numberPressed(button: UIButton!) {
+    @IBAction func numberPressed(sender: UIButton!) {
         playSound()
+        
+        addNumberToRunningTotal("\(sender.tag)")
+        updateDisplay(runningNumber)
     }
     
-    func loadSound() {
+    @IBAction func clearButtonPressed(sender: UIButton) {
+        processOperation(Operation.Clear)
+    }
+    
+    @IBAction func divideButtonPressed(sender: UIButton) {
+        processOperation(Operation.Divide)
+    }
+    
+    @IBAction func multiplyButtonPressed(sender: UIButton) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func subtractButtonPressed(sender: UIButton) {
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func addButtonPressed(sender: UIButton) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func equalsButtonPressed(sender: UIButton) {
+        processOperation(currentOperation)
+    }
+    
+    internal func processOperation(operation: Operation) {
+        playSound()
+        
+        if !isReadyToPerformAnOperation() {
+            currentOperation = operation
+            return
+        }
+        
+        if isThereACurrentOperation() {
+            rightCalculationPart = runningNumber
+            runningNumber = ""
+            
+            if operation == Operation.Divide {
+                result = "\(Double(leftCalculationPart)! / Double(rightCalculationPart)!)"
+            } else if operation == Operation.Multiply {
+                result = "\(Double(leftCalculationPart)! * Double(rightCalculationPart)!)"
+            } else if operation == Operation.Subtract {
+                result = "\(Double(leftCalculationPart)! - Double(rightCalculationPart)!)"
+            } else if operation == Operation.Add {
+                result = "\(Double(leftCalculationPart)! + Double(rightCalculationPart)!)"
+            }
+            
+            leftCalculationPart = result
+            updateDisplay(result)
+        } else {
+            leftCalculationPart = runningNumber
+            runningNumber = ""
+        }
+        
+        currentOperation = operation
+    }
+    
+    internal func loadSound() {
         let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
         let url = NSURL.fileURLWithPath(path!)
         
@@ -41,13 +116,28 @@ class ViewController: UIViewController {
         }
     }
     
-    func playSound()
-    {
+    internal func playSound() {
         if (buttonSound.playing) {
             buttonSound.stop()
         }
         
         buttonSound.play()
+    }
+    
+    internal func updateDisplay(value: String) {
+        display.text = value
+    }
+    
+    internal func addNumberToRunningTotal(number: String) {
+        runningNumber += number
+    }
+    
+    internal func isThereACurrentOperation() -> Bool {
+        return currentOperation != Operation.None
+    }
+    
+    internal func isReadyToPerformAnOperation() -> Bool {
+        return runningNumber != ""
     }
 }
 
