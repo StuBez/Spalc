975a540f317561283a2d38a9feacffb05218230b
Stuart Chapman <stubez@users.noreply.github.com>
2015-09-30 13:17:26 +0100

    Initial Commit

---
+++ Spalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spalc
+//
+//  Created by Stuart Chapman on 30/09/2015.
+//  Copyright © 2015 little robot Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
